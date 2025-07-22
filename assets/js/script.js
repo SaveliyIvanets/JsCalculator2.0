f5bc586aa9b77412848c7b6fdc84812bfb125c5e
Saveliy Ivanets <ivanets300@mail.ru>
2025-07-22 19:07:25 +0300

    rework 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,18 +30,6 @@ function outputSTArray(arr) {
   }
   return ans.slice(0, ans.length - 1);
 }
-
-function formatCurrentDateTime() {
-  const now = new Date();
-  const day = String(now.getDate()).padStart(2, "0");
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const year = now.getFullYear();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`; // Шаблонный литерал
-}
-
 function averageArray(array) {
   let sum = 0;
   for (let x of array) {
